c521b4a9cedb252599d4749293c208d043ef7bab
Logan Brown <logmbrow@ut.utm.edu>
2021-03-23 12:50:27 -0700

    Add project files.

---
+++ Threading/FIndPiThread.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threading
+{
+    public class FindPiThread
+    {
+        int darts;
+        int dartsLanded { get; set; }
+        double randThrows;
+        System.Random ranNum = new System.Random();
+
+        public FindPiThread(int darts_to_throw)
+        {
+            darts = darts_to_throw;
+            randThrows = ranNum.Next(0, 1);
+        }
+
+
+        public void throwDarts()
+        {
+            double x = randThrows;
+            double y = ranNum.Next(0, 1);
+
+        }
+    }
+}
